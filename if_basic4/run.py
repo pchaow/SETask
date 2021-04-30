975bf0804efcf2b00cb22ff4f02fd4b620a425b1
Chao Porkaew <chaow.po@up.ac.th>
2021-04-30 05:16:59 +0000

    update stupid

--- if_basic4/run.py
+++ if_basic4/run.py
@@ -9,7 +9,7 @@ template = 'template1.py'
 
 thecode = input.parse_template(template, studentfile)
 score = 0 
-total = 2
+total = 3
 
 # TEST 1
 input1 = """3
@@ -31,9 +31,11 @@ stdout = stdout.strip()
 if stdout ==output1 :
     score += 1
     feedback.set_global_feedback("""CASE 1 : ถูกครับ
+    
     """,append=True)
 else :
     feedback.set_global_feedback("""CASE 1 : ผิดครับ
+
     """,append=True)
 
 
@@ -56,10 +58,40 @@ stdout = stdout.strip()
 if stdout == output2 :
     score += 1
     feedback.set_global_feedback("""CASE 2 : ถูกครับ
+
     """,append=True)
 else :
     feedback.set_global_feedback("""CASE 2 : ผิดครับ
+
     """,append=True)
+
+
+# TEST 3
+
+input3 = """10
+5
+15
+"""
+output3 = "15"
+
+stdout, stderr, stdretval = run_student_simple(
+    f"python3 {studentfile}",
+    cmd_input=input3, 
+    time_limit=30
+    )
+
+stdout = stdout.strip()
+
+if stdout == output3 :
+    score += 1
+    feedback.set_global_feedback("""CASE 3 : ถูกครับ
+
+    """,append=True)
+else :
+    feedback.set_global_feedback("""CASE 3 : ผิดครับ
+
+    """,append=True)
+
 
 
 if score == total :
